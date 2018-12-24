834e9528e4c8cfd367b8cd75c45d8a396392333e
pranavkamat11 <pranavkamat11@gmail.com>
2018-12-25 05:13:35 +0530

    dealing with numbers

---
+++ python tutorial/integersAndFloats.py
@@ -0,0 +1,17 @@
+num = 3.14
+print(type(num))
+print(abs(-3))
+print(round(3.75,1))
+
+# returns a boolean
+num1 = 3
+num2 = 2
+print(num1 == num2)
+print(num1 >= num2)
+
+num3 = '100'
+num4 = '200'
+
+num3 = int(num3)
+num4 = int(num4)
+print(num3 + num4)
