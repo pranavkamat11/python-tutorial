4e80a04e13adf0d7fd348a902ebbee8154c7cff8
pranavkamat11 <pranavkamat11@gmail.com>
2018-12-25 04:56:11 +0530

    placement of parameters in string etc

--- python tutorial/workingWIthTexualData.py
+++ python tutorial/workingWIthTexualData.py
@@ -1,3 +1,27 @@
-message = "hello pranav"
+message = "Hello pranav"
 print(message)
 print(len(message))
+print(message[0])
+print(message[0:4])
+print(message[:4])
+print(message[6:10])
+print(message.lower())
+print(message.upper())
+print(message.count('a'))
+print(message.count('Hello'))
+print(message.find('p'))
+print(message.find('pranav'))
+print(message.find('universe'))
+
+# replace message
+message = message.replace('pranav','Santosh')
+print(message)
+
+message = message + ' ' + 'Kamat'
+print(message)
+
+# newer way
+name = "gayatri"
+surname = "kamat"
+message = "{} {} , how are you ".format(name,surname)
+print(message)
