3e0c3c0eb71928402fdffc051bfb7c7d296a8690
pranavkamat11 <pranavkamat11@gmail.com>
2018-12-25 05:38:38 +0530

    starting with lists

---
+++ python tutorial/ListTupplesAndSets.py
@@ -0,0 +1,11 @@
+# List and tupples allow to work with sequential data
+
+courses = ['history','Maths','computer']
+print(courses)
+print(len(courses))
+print(courses[0])
+
+# if list is too huge no need to worry about length 
+print(courses[-1])
+
+# Set allows to work with collection of values with no duplicates
