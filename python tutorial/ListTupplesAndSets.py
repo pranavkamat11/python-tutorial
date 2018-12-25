616bea929448e9fb388ab6dcd56794b717ae500b
pranavkamat11 <pranavkamat11@gmail.com>
2018-12-25 05:53:26 +0530

    playing around with lists

--- python tutorial/ListTupplesAndSets.py
+++ python tutorial/ListTupplesAndSets.py
@@ -5,7 +5,10 @@ print(courses)
 print(len(courses))
 print(courses[0])
 
-# if list is too huge no need to worry about length 
+# if list is too huge no need to worry about length use [-1]
 print(courses[-1])
+
+print(courses[0:2])
+print(courses[1:])
 
 # Set allows to work with collection of values with no duplicates
