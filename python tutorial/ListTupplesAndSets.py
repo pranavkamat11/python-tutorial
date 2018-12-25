7f516999335192a25034146a07ef829629c86ba5
pranavkamat11 <pranavkamat11@gmail.com>
2018-12-25 18:01:30 +0530

    pop and remove method lists

--- python tutorial/ListTupplesAndSets.py
+++ python tutorial/ListTupplesAndSets.py
@@ -22,4 +22,13 @@ courses_2 = ["Machine Learning","Artificial Intelligence"]
 courses.extend(courses_2)
 print(courses)
 
+courses = ['history','Maths','computer']
+courses.remove('Maths')
+print(courses)
+
+# Using list like stack . ie removing the last element
+courses = ['history','Maths','computer']
+courses.pop()
+print(courses)
+
 # Set allows to work with collection of values with no duplicates
