a995d91dc506708441eb82be8096b584f8e80cff
pranavkamat11 <pranavkamat11@gmail.com>
2018-12-28 04:21:24 +0530

    using random module from standard library

---
+++ standardLibrary.py
@@ -0,0 +1,7 @@
+# importing random module from standard library
+import random
+
+courses = ["Math","Science","Geography","Chemistry"]
+
+random_choice = random.choice(courses)
+print(random_choice)
